c6e07c706e6423ad8f5a3527560e2fd482157376
Eduardo França <francaedu98@gmail.com>
2021-06-29 20:42:26 -0300

    Validations messages class config

--- src/main/java/com/avaliacaobackend/domain/config/ValidationsMessagesConfig.java
+++ src/main/java/com/avaliacaobackend/domain/config/ValidationsMessagesConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.support.ReloadableResourceBundleMessageSource
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
-public class WebMvcConfig {
+public class ValidationsMessagesConfig {
 
     @Bean
     public MessageSource messageSource() {
